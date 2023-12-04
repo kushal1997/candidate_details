f0aa4b80effb151fe939c0b01fc03ae5c66d67c7
kushal1997 <kushalrao103@gmail.com>
2023-12-04 15:44:48 +0530

    ENJOY

--- src/candidate/Candidate.js
+++ src/candidate/Candidate.js
@@ -8,10 +8,7 @@ import moment from 'moment-timezone';
 import { useNavigate } from 'react-router-dom';
 
 const validationSchema = Yup.object().shape({
-  recruiter_name: Yup.string()
-    .min(2, 'Too short!')
-    .max(20, 'too Long!')
-    .required('Full name is required'),
+  
   first_name: Yup.string()
     .min(2, 'Too short!')
     .max(20, 'too Long!')
@@ -198,25 +195,25 @@ const Candidate = () => {
           </div>
 
           <div className="flex w-full flex-row justify-evenly">
-            <a href="https://docs.google.com/spreadsheets/d/1VTjAwvtufsq9JJjjrKcjtA-fBppqlzHtlQXP5AiymJc/edit?usp=sharing" 
-            className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
-            target="_blank" rel="noopener noreferrer">
+            <a href="https://docs.google.com/spreadsheets/d/1VTjAwvtufsq9JJjjrKcjtA-fBppqlzHtlQXP5AiymJc/edit?usp=sharing"
+              className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
+              target="_blank" rel="noopener noreferrer">
               Pushpanjali
             </a>
-            <a href="https://docs.google.com/spreadsheets/d/14ksJ9U6rfQgbhRrpOHJudUw77UqZ_AsHrpZ_fXcE-EQ/edit?usp=sharing" 
-            className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
-            target="_blank" rel="noopener noreferrer">
-              Rituparna 
+            <a href="https://docs.google.com/spreadsheets/d/14ksJ9U6rfQgbhRrpOHJudUw77UqZ_AsHrpZ_fXcE-EQ/edit?usp=sharing"
+              className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
+              target="_blank" rel="noopener noreferrer">
+              Rituparna
             </a>
-            <a href="https://docs.google.com/spreadsheets/d/1CC52c_E4-VUbebek3Q163DEplkJoTyDVnWTzM8p0YOI/edit?usp=sharing" 
-            className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
-            target="_blank" rel="noopener noreferrer">
-              Rajlaxmi 
+            <a href="https://docs.google.com/spreadsheets/d/1CC52c_E4-VUbebek3Q163DEplkJoTyDVnWTzM8p0YOI/edit?usp=sharing"
+              className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
+              target="_blank" rel="noopener noreferrer">
+              Rajlaxmi
             </a>
-            <a href="https://docs.google.com/spreadsheets/d/1jZ_vTGYOQQ4uHx55ngpwCKI-vsZDjRLop2C_5cw5tos/edit?usp=sharing" 
-            className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
-            target="_blank" rel="noopener noreferrer">
-              Dharitri 
+            <a href="https://docs.google.com/spreadsheets/d/1jZ_vTGYOQQ4uHx55ngpwCKI-vsZDjRLop2C_5cw5tos/edit?usp=sharing"
+              className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-3 py-1.5 text-center me-2 mb-2 mt-8'
+              target="_blank" rel="noopener noreferrer">
+              Dharitri
             </a>
           </div>
           <div className={`text-white font-poppins text-[16px] border-2 mt-10 h-fit bg-blue-900 shadow-lg shadow-blue-500  items-center overflow-hidden ${styles.boxWidth}`}>
